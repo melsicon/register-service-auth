828efb65088e61470a69bd2f85e9f0530e25525c
Marvin Bernd <hello@marvinbernd.de>
2021-07-16 09:46:42 +0200

    add docker

--- routes/register.js
+++ routes/register.js
@@ -10,12 +10,12 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).send({ message: error.details[0].message });
 
   const { email, firstName, lastName, password } = req.body;
 
   if (users.find((user) => user.email === email)) {
-    return res.status(400).send("User already registered.");
+    return res.status(400).send({ message: "User already registered." });
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -28,7 +28,7 @@ router.post("/", async (req, res) => {
     password: hashedPassword,
   });
 
-  return res.status(200).send("User successfully registered.");
+  return res.status(200).send({ message: "User successfully registered." });
 });
 
 function validate(req) {
